e91221ac496c95583a1302c13cbf931e7e471282
irvinzato <multizato@hotmail.com>
2022-05-09 18:59:51 -0500

    Se trabajaron los ciclos de vida mas utilizados en Angular, en app componente se utilizo un boton para probar ngDrestroy y una manera interesante de utilizar el evento click solo para cambiar un true o false, ademas de una condicion interesante para mostrar en html un boton u otro, como si fuera un if. En pagina1 hay mas componentes dentro del html para probar mas ciclos de vida y formas correctas para crear y parar observables, ademas del uso del decorador Input para activar ngOnChanges, alli ejemplos con comentarios

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,6 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-pagina1',
@@ -8,6 +10,10 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
                         AfterViewInit, AfterViewChecked, OnDestroy {
 
+  nombre:string = "Irving";
+  segundos:number = 0;
+  timerSubscription: Subscription;
+                      
   constructor() { 
     console.log("Soy el contructor");
   }
@@ -38,10 +44,20 @@ export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterConten
   
   ngOnDestroy(): void {
     console.log('Soy ngOnDestroy');
+    this.timerSubscription.unsubscribe();  //Aqui es cuando le digo que pare ese Observable al destruir el componente
+    console.log("Limpieza de Observable");
   }
 
   ngOnInit() {
     console.log("Soy ngOnInit");
+    this.timerSubscription = interval(2000).subscribe( i => { //Cuando es un Observable infinito, tenemos que decirle cuando para, si no siempre seguira ejetucando
+      console.log("Valor de mi suscripcion en interval ", i);   //No se ve claro en el HTML pero si en la consola
+      this.segundos = i;
+    });
+  }
+
+  guardar(){
+
   }
 
 }
